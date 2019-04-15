bfb573b2587d09d3c5c389c06f7eec90f6cd1e07
JeremyLIU1997 <43444842+JeremyLIU1997@users.noreply.github.com>
2019-04-15 17:01:32 +0800

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -23,6 +23,7 @@ def init():
 	arrival_queue.append(Event(4,1,3,8))
 	arrival_queue.append(Event(5,7,4,4))
 	"""
+
 	arrival_queue.append(Event(1,9,0,4))
 	arrival_queue.append(Event(2,5,1,7))
 	arrival_queue.append(Event(3,2,2,2))
@@ -60,58 +61,6 @@ def compare_wrapper(mycmp):
         def __ne__(self, other):
             return mycmp(self.obj, other.obj) != 0
     return K
-
-def SRT(arrival_queue):
-	current_t = 0
-	arrival_queue_head = 0
-	run_queue = []
-	while (True):
-		if arrival_queue_head < len(arrival_queue) and current_t >= arrival_queue[arrival_queue_head].arrival_t:
-			run_queue.append(arrival_queue[arrival_queue_head])
-			run_queue = sorted(run_queue, key = compare_wrapper(remaining_time_compare))
-			arrival_queue_head+=1
-		if (len(run_queue) == 0 ):
-			schedule.append(-1)
-		else:
-			schedule.append(run_queue[0].id)
-			run_queue[0].burst_t -= 1
-			if (run_queue[0].burst_t == 0):
-				run_queue.pop(0)
-		current_t+=1
-		
-		if arrival_queue_head == len(arrival_queue) and len(run_queue) == 0:
-			break;
-	return schedule
-
-
-
-def priority_with_premption(arrival_queue,option):
-	current_t = 0
-	arrival_queue_head = 0
-	run_queue = []
-	if option == "linux":
-		reverse_flag = False
-	elif option == "win":
-		reverse_flag = True
-
-	while (True):
-		if arrival_queue_head < len(arrival_queue) and current_t >= arrival_queue[arrival_queue_head].arrival_t:
-			run_queue.append(arrival_queue[arrival_queue_head])
-			run_queue = sorted(run_queue, key = lambda x: x.priority, reverse = reverse_flag)
-			arrival_queue_head+=1
-		if (len(run_queue) == 0):
-			schedule.append(-1)
-		else:
-			schedule.append(run_queue[0].id)
-			run_queue[0].burst_t -= 1
-			if (run_queue[0].burst_t == 0):
-				run_queue.pop(0)
-		current_t+=1
-		
-		if arrival_queue_head == len(arrival_queue) and len(run_queue) == 0:
-			break;
-	return schedule
-
 
 def print_result(arrival_queue):
 	decision_count = 0
@@ -204,6 +153,54 @@ def RR(arrival_queue,q): # q stands for quantum
 				
 	return schedule
 
+def SRT(arrival_queue):
+	current_t = 0
+	arrival_queue_head = 0
+	run_queue = []
+	while (True):
+		if arrival_queue_head < len(arrival_queue) and current_t >= arrival_queue[arrival_queue_head].arrival_t:
+			run_queue.append(arrival_queue[arrival_queue_head])
+			run_queue = sorted(run_queue, key = compare_wrapper(remaining_time_compare))
+			arrival_queue_head+=1
+		if (len(run_queue) == 0 ):
+			schedule.append(-1)
+		else:
+			schedule.append(run_queue[0].id)
+			run_queue[0].burst_t -= 1
+			if (run_queue[0].burst_t == 0):
+				run_queue.pop(0)
+		current_t+=1
+		
+		if arrival_queue_head == len(arrival_queue) and len(run_queue) == 0:
+			break
+	return schedule
+
+def priority_with_premption(arrival_queue,option):
+	current_t = 0
+	arrival_queue_head = 0
+	run_queue = []
+	if option == "linux":
+		reverse_flag = False
+	elif option == "win":
+		reverse_flag = True
+
+	while (True):
+		if arrival_queue_head < len(arrival_queue) and current_t >= arrival_queue[arrival_queue_head].arrival_t:
+			run_queue.append(arrival_queue[arrival_queue_head])
+			run_queue = sorted(run_queue, key = lambda x: x.priority, reverse = reverse_flag)
+			arrival_queue_head+=1
+		if (len(run_queue) == 0):
+			schedule.append(-1)
+		else:
+			schedule.append(run_queue[0].id)
+			run_queue[0].burst_t -= 1
+			if (run_queue[0].burst_t == 0):
+				run_queue.pop(0)
+		current_t+=1
+		
+		if arrival_queue_head == len(arrival_queue) and len(run_queue) == 0:
+			break
+	return schedule
 
 if __name__ == '__main__':
 	arrival_queue = init()
